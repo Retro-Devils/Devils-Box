644288391e645a1f33bee54dea64863a51f363e2
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-09 15:35:32 +0000

    Add files via upload

---
+++ Thing/.full/coco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+read -p "Do You Wish To Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  echo "Starting Download";
+else
+  echo "Exiting Now";sudo reboot
+fi
+cd
+mkdir test
+cd test
+wget -c https://archive.org/download/thing-box/Retro-Devils-Coco.7z -q --show-progress
+7z x -y -aoa /home/pi/test/Retro-Devils-Coco.7z
+sudo mv coco /home/pi/RetroPie/roms
+sudo rm "Retro-Devils-Coco.7z"
+cd ..
+sudo rm -R test
+sudo reboot
+
+
