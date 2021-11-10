a4e58c76ae707ebc266cbec49bce46657340d5a6
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-09 23:35:29 -0500

    Add files via upload

---
+++ Thing/.full/snes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+wget --spider https://archive.org/download/thing-box/Retro-Devils-SNES.7z
+read -p "Do You Wish To Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  echo "Starting Download";
+else
+  echo "Rebooting Now";sudo reboot
+fi
+sudo rm -r /home/pi/RetroPie/roms/snes
+cd
+mkdir test
+cd test
+wget -c https://archive.org/download/thing-box/Retro-Devils-SNES.7z -q --show-progress
+7z x -y -aoa /home/pi/test/Retro-Devils-SNES.7z
+sudo mv snes /home/pi/RetroPie/roms
+sudo rm "Retro-Devils-SNES.7z"
+cd ..
+sudo rm -R test
+sudo reboot
+
+
+
