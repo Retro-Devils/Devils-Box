af779bd41639fb2691f10c5d164324a4d3775148
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-09 20:40:25 -0500

    Update Hide OR Show Devils Box.sh

--- ToolBox/Hide OR Show Devils Box.sh
+++ ToolBox/Hide OR Show Devils Box.sh
@@ -9,14 +9,14 @@ fi
 if [ -d "/home/pi/RetroPie/retropiemenu/Devils-Box/kiosk" ] 
 then
 sudo rm -R "/home/pi/RetroPie/retropiemenu/Devils-Box/kiosk"
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Full-Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.full
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Hacks-Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.hacks
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Remove-Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.remove
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Full Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.full
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Hacks Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.hacks
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Remove Packs /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.remove
 else
 mkdir /home/pi/RetroPie/retropiemenu/Devils-Box/kiosk
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.full /home/pi/RetroPie/retropiemenu/Devils-Box/Full-Packs
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.hacks /home/pi/RetroPie/retropiemenu/Devils-Box/Hacks-Packs
-mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.remove /home/pi/RetroPie/retropiemenu/Devils-Box/Remove-Packs
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.full /home/pi/RetroPie/retropiemenu/Devils-Box/Full Packs
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.hacks /home/pi/RetroPie/retropiemenu/Devils-Box/Hacks Packs
+mv /home/pi/RetroPie/retropiemenu/Devils-Box/Thing/.remove /home/pi/RetroPie/retropiemenu/Devils-Box/Remove Packs
 fi
 touch /tmp/es-restart
-kill -15 $(pidof emulationstation)
+sudo reboot
