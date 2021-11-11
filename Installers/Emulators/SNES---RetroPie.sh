8c931a23b4cf1034cc6ae178b5cc68fc6ea15f98
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-11 11:41:35 -0500

    Add files via upload

---
+++ Installers/Emulators/SNES---RetroPie.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/install-launch.txt
+read -p "Do You Wish To Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  echo "Starting Install";
+else
+  echo "Exiting Now";sudo reboot
+fi
+sudo ./retropie_packages lr-snes9x2002
+sudo ./retropie_packages lr-snesx9x
+sudo reboot
