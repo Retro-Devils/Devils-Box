8a377c1613ddb47e46d61d9bb73304edd80014d7
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-11 11:12:36 -0500

    Update amigacd32.sh

--- Thing/.full/amigacd32.sh
+++ Thing/.full/amigacd32.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+wget --spider https://archive.org/download/thing-box/Retro-Devils-AmigaCD.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Rebooting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/amigacd32
 cd
 mkdir test
 cd test
@@ -15,6 +18,8 @@ sudo mv amigacd32 /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-AmigaCD.7z"
 cd ..
 sudo rm -R test
+echos"Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
