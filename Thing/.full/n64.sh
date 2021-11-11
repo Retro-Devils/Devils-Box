ad1781ea689e0063bbd0108adbdc0d549161a565
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-11 06:20:52 +0000

    Update n64.sh

--- Thing/.full/n64.sh
+++ Thing/.full/n64.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+wget --spider https://archive.org/download/thing-box/Retro-Devils-n64.7z
+echo "This Will Also Install Emulator(s)"
+echo "Above Is File Size & Info"
+echo "N64 Will Take A Looong Time"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Exiting Now";sudo reboot
 fi
+echo "Installing Emulator(s) Now"
+sleep 2
+sudo bash /home/pi/RetroPie-Setup/retropie_packages.sh lr-parallel-n64
+sudo bash /home/pi/RetroPie-Setup/retropie_packages.sh lr-mupen64pus
+sudo bash /home/pi/RetroPie-Setup/retropie_packages.sh mupen64plus
+echo "Installing & Cleaning Everything Else Now"
+sleep 2
 cd
 mkdir test
 cd test
@@ -15,6 +26,8 @@ sudo mv n64 /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-n64.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
