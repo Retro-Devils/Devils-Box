cdd90fc3dda081c9abe389c1aa35949824e5af0e
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-11 11:08:53 -0500

    Update amiga.sh

--- Thing/.full/amiga.sh
+++ Thing/.full/amiga.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
 wget --spider https://archive.org/download/thing-box/Retro-Devils-amiga.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Rebooting Now";sudo reboot
 fi
+10sudo rm -r /home/pi/RetroPie/roms/amiga
 cd
 mkdir test
 cd test
