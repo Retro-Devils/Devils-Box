7398aa986cecfd045cf3ec28b395a055cc29d652
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-11 11:01:35 -0500

    Update nes.sh

--- Thing/.full/nes.sh
+++ Thing/.full/nes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
 wget --spider https://archive.org/download/thing-box/Retro-Devils-NES.7z
-echo "This Will Also Install Emulator(s)"
 echo "Above Is File Size & Info"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
@@ -9,11 +8,6 @@ if [ "$CONT" = "y" ]; then
 else
   echo "Exiting Now";sudo reboot
 fi
-echo "Installing Emulator(s) Now"
-sleep 2
-sudo bash /home/pi/RetroPie-Setup/retropie_packages.sh lr-nestopia
-echo "Installing & Cleaning Everything Else Now"
-sleep 2
 sudo rm -r /home/pi/RetroPie/roms/nes
 cd
 mkdir test
@@ -27,5 +21,4 @@ sudo rm -R test
 echo "Complete Rebooting Now"
 sleep 5
 sudo reboot
-
 
