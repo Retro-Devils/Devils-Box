6a82074dd5d87f92f4e06112e0f589cdb8a4635c
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-12 23:44:08 +0000

    Update Argon1-Script-Installer.sh

--- ToolBox/Hardware/Cases/Argon1-Script-Installer.sh
+++ ToolBox/Hardware/Cases/Argon1-Script-Installer.sh
@@ -7,9 +7,9 @@ else
   echo "Exiting Now";
 fi
 curl https://download.argon40.com/argon1.sh | bash
-cat /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/a1.txt
+cat /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Cases/a1.txt
 sleep 6
-sudo rm /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Argon1-Script-Installer.sh
-sudo rm /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/a1.txt
-sudo mv /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/.Argon1-Config.sh /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Argon1-Config.sh
+sudo rm /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Cases/Argon1-Script-Installer.sh
+sudo rm /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Casesa1.txt
+sudo mv /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Cases.Argon1-Config.sh /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Cases/Argon1-Config.sh
 sudo reboot
