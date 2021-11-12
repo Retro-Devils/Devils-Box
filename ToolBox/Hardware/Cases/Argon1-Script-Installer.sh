8328ee3f60137b470cc4c8aa50f533ab12487704
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-12 23:45:42 +0000

    Update Argon1-Script-Installer.sh

--- ToolBox/Hardware/Cases/Argon1-Script-Installer.sh
+++ ToolBox/Hardware/Cases/Argon1-Script-Installer.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/tools-launch.txt
-read -p "Do You Wish To Continue (y/n)?" CONT
-if [ "$CONT" = "y" ]; then
-  echo "Starting Tool";
-else
-  echo "Exiting Now";
-fi
 curl https://download.argon40.com/argon1.sh | bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/ToolBox/Hardware/Cases/a1.txt
 sleep 6
