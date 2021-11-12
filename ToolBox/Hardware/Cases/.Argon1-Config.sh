f7137294ffc068bbdf9b2f484b7b2e8e7c827037
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-12 23:49:36 +0000

    Update .Argon1-Config.sh

--- ToolBox/Hardware/Cases/.Argon1-Config.sh
+++ ToolBox/Hardware/Cases/.Argon1-Config.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/tools-launch.txt
-read -p "Do You Wish To Continue (y/n)?" CONT
-if [ "$CONT" = "y" ]; then
-  echo "Starting Tool";
-else
-  echo "Exiting Now";
-fi
 argonone-config
