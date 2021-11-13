708a54c500f2a1258c6c130b9464bf6eb3f7b266
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 03:46:31 +0000

    Update arcadia.sh

--- Thing/.full/arcadia.sh
+++ Thing/.full/arcadia.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
-read -p "Do You Wish To Continue (y/n)?" CONT
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-Arcadia.7z
+echo "Above Is File Info & Size"read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Rebooting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/arcadia
 cd
 mkdir test
 cd test
@@ -15,6 +18,8 @@ sudo mv arcadia /home/pi/RetroPie/roms
 sudo rm "Retro-Devils_Arcadia.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
