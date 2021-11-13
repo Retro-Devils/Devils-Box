b3e96eb0426dc49ec553345872cee6f7027f830b
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 22:47:29 +0000

    Update supergrafx.sh

--- Thing/.full/supergrafx.sh
+++ Thing/.full/supergrafx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
 wget --spider https://archive.org/download/thing-box/Retro-Devils-Supergrafx.7z
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
@@ -7,16 +8,18 @@ if [ "$CONT" = "y" ]; then
 else
   echo "Rebooting Now";sudo reboot
 fi
+sudo rm - r /home/pi/RetroPie/roms/supergrafx
 cd
 mkdir test
 cd test
 wget -c https://archive.org/download/thing-box/Retro-Devils-Supergrafx.7z -q --show-progress
 7z x -y -aoa /home/pi/test/Retro-Devils-Supergrafx.7z
-sudo rm - r /home/pi/RetroPie/roms/supergrafx
 sudo mv supergrafx /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-Supergrafx.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
