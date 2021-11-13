c91cf505727f46dea11f9c146406ca0956effee2
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 03:57:01 +0000

    Update bbcmicro.sh

--- Thing/.full/bbcmicro.sh
+++ Thing/.full/bbcmicro.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-bbcmicro.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Exiting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/bbcmicro
 cd
 mkdir test
 cd test
@@ -15,6 +19,7 @@ sudo mv bbcmicro /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-bbcmicro.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
-
 
