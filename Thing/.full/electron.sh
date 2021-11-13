0312681f75fde0380fa35562b19ee1b7f49a9eec
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 04:09:26 +0000

    Update electron.sh

--- Thing/.full/electron.sh
+++ Thing/.full/electron.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-Electron.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Exiting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/electron
 cd
 mkdir test
 cd test
@@ -15,6 +19,7 @@ sudo mv electron /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-Electron.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
-
 
