a41e52de7d75267cd27dfbdd76dd5aa7ab95f918
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 22:12:37 +0000

    Update gb.sh

--- Thing/.full/gb.sh
+++ Thing/.full/gb.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-gb.7z
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Exiting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/gb
 cd
 mkdir test
 cd test
@@ -15,6 +18,8 @@ sudo mv gb /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-gb.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
