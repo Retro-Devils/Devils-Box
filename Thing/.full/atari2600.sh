9fab5f4e009206b721524b94383f1de478400b8b
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 03:48:44 +0000

    Update atari2600.sh

--- Thing/.full/atari2600.sh
+++ Thing/.full/atari2600.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-Atari2600.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Rebooting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/atari2600
 cd
 mkdir test
 cd test
@@ -15,6 +19,8 @@ sudo mv atari2600 /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-Atari2600.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
