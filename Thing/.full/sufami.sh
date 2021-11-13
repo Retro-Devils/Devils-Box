02b48ed2ab106a2d0da88babdaee38f8eda771bf
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 22:46:18 +0000

    Update sufami.sh

--- Thing/.full/sufami.sh
+++ Thing/.full/sufami.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
 wget --spider https://archive.org/download/thing-box/Retro-Devils-Sufami.7z
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
@@ -7,16 +8,18 @@ if [ "$CONT" = "y" ]; then
 else
   echo "Rebooting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/sufami
 cd
 mkdir test
 cd test
 wget -c https://archive.org/download/thing-box/Retro-Devils-Sufami.7z -q --show-progress
-sudo rm -r /home/pi/RetroPie/roms/sufami
 7z x -y -aoa /home/pi/test/Retro-Devils-Sufami.7z
 sudo mv sufami /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-Sufami.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
