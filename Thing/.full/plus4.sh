b2b7a9657de76c9824427c68ed7fa66ae7418bf0
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-11-13 22:35:17 +0000

    Update plus4.sh

--- Thing/.full/plus4.sh
+++ Thing/.full/plus4.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cat /home/pi/RetroPie/retropiemenu/Devils-Box/setup/txts/packs-launch.txt
+sleep 2
+wget --spider https://archive.org/download/thing-box/Retro-Devils-Plus4.7z
+echo "Above Is File Info & Size"
 read -p "Do You Wish To Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Starting Download";
 else
   echo "Exiting Now";sudo reboot
 fi
+sudo rm -r /home/pi/RetroPie/roms/plus4
 cd
 mkdir test
 cd test
@@ -15,6 +19,8 @@ sudo mv plus4 /home/pi/RetroPie/roms
 sudo rm "Retro-Devils-Plus4.7z"
 cd ..
 sudo rm -R test
+echo "Complete Rebooting Now"
+sleep 5
 sudo reboot
 
 
