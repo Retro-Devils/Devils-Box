197e89ccce752c5da8b836a1e704fb1a02b3ea73
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2021-12-03 05:22:56 +0000

    Delete Installers directory

--- Installers/Emulators/Mugen---Supreme-Team.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "This Is A Mugen Installer Built by Supreme Team and others"
-sleep 3
-curl -sSL https://git.io/Jz9O3 | bash
--- Installers/RetroPie-Extras---ZeroJay.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~
-git clone https://github.com/zerojay/RetroPie-Extra.git
-cd RetroPie-Extra/
-./install-extras.sh
