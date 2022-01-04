8a5239ad6e39d2f69e7c3eff27a86c2162061e92
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-04 14:16:15 -0500

    Add files via upload

---
+++ DB-Install.sh
@@ -0,0 +1,55 @@
+ #!/bin/bash
+
+export NCURSES_NO_UTF8_ACS=1
+###----------------------------###
+### INSTALLER MENU FUNCTIONS   ###
+###----------------------------###
+
+function main_menu() {
+    local choice
+
+    while true; do
+        choice=$(dialog --backtitle "$BACKTITLE" --title " DEVILS BOX 2.0 " \
+            --ok-label Select --cancel-label Exit-Installer \
+            --menu "
+DEVILS BOX RETROPIE INSTALLER
+--------------------------------------
+WHATS THIS?    A DOWNLOADER & TOOL BOX 
+--------------------------------------
+CREATED BY?    THE RETRO DEVILS & OTHERS
+-------------------------------------- 
+CREATED WHEN?  2021/2022
+--------------------------------------
+SYSTEM WILL REBOOT WHEN COMPLETE" 25 50 40 \
+            1 "Install Devils Box " \
+            2 "Update Devils Box  " \
+            2>&1 > /dev/tty)
+
+        case "$choice" in
+            1) install_db      ;;
+            2) update_db       ;;
+            *)  break ;;
+        esac
+    done
+}
+#---------------------------#
+#INSTALL DEVILS BOX #
+#---------------------------#
+function install_db() {
+git clone https://github.com/Retro-Devils/Devils-Box -P ~/
+mv ~/Devils-Box/Devils-box.sh -P ~/RetroPie/retropiemenu/
+sudo reboot
+}
+#------------------#
+#UPDATE DEVILS BOX #
+#------------------#
+function update_db() {
+sudo rm ~/RetroPie/retropiemenu/Devils-Box.sh
+sudo rm -R ~/Devils-Box/
+git clone https://github.com/Retro-Devils/Devils-Box -P ~/
+mv ~/Devils-Box/Devils-box.sh -P ~/RetroPie/retropiemenu/
+sudo reboot
+}
+
+# Main
+main_menu
