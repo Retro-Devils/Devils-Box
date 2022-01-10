ead5480725aa9049fbac0c8581e1040b2bd48f8a
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-01-09 18:53:36 -0600

    Update DB-Install.sh

--- DB-Install.sh
+++ DB-Install.sh
@@ -38,6 +38,7 @@ if [ -f "$HOME/RetroPie/retropiemenu/Devils-Box.sh" ]; then sudo rm ~/RetroPie/r
 if [ -d "$HOME/Devils-Box/" ]; then sudo rm -R ~/Devils-Box/; fi
 git clone https://github.com/Retro-Devils/Devils-Box
 cp ~/Devils-Box/Devils-Box.sh -f ~/RetroPie/retropiemenu/
+cp $HOME/Devils-Box/files/Devils-Box.png -f ~/RetroPie/retropiemenu/icons/
 chmod 755 ~/RetroPie/retropiemenu/Devils-Box.sh
 if [ ! -s ~/RetroPie/retropiemenu/gamelist.xml ]; then sudo rm -f ~/RetroPie/retropiemenu/gamelist.xml; fi
 if [ ! -f "~/RetroPie/retropiemenu/gamelist.xml" ]; then cp /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ~/RetroPie/retropiemenu/gamelist.xml; fi
