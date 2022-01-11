4d01de06626bc215ae1f571e86d7f979467f855c
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-11 12:41:13 -0500

    Update DB-Remote.sh

--- DB-Remote.sh
+++ DB-Remote.sh
@@ -14,7 +14,7 @@ function main_menu() {
   local choice
 
   while true; do
-    choice=$(dialog --backtitle "$BACKTITLE" --title " REMOTE DEVILS BOX " \
+    choice=$(dialog --backtitle "$BACKTITLE" --title " REMOTE DEVILS BOX V2.2.   1/11/22" \
       --ok-label Select --cancel-label Exit-Devils-Box \
       --menu "DEVILS BOX MAIN MENU" 25 50 40 \
       1 "Console Packs" \
