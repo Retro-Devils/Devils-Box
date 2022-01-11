7c94f9f18b97963d7d68c54f17540b4d99865bbf
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-11 21:35:01 +0000

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -703,7 +703,7 @@ function update_db() {
   sleep 1
   cp ~/Devils-Box/files/box -f /usr/local/bin/
   sleep 2
-  sudo chmod 755 /usr/local/bin/box
+  chmod 755 /usr/local/bin/box
   sleep 1
   bash ~/RetroPie/retropiemenu/Devils-Box.sh
   exit 1
