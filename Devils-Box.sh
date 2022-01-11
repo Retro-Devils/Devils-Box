fbd2a45c5e3ce7a44dc9351c0772db64986f90e6
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-11 07:23:54 +0000

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -701,7 +701,7 @@ function update_db() {
   cd ~/Devils-Box
   git pull -f
   cp ~/Devils-Box/Devils-Box.sh -f ~/RetroPie/retropiemenu/
-  cp ~/Devils-Box/files/DB -f ~/
+  cp ~/Devils-Box/files/DB.sh -f ~/
   sleep 1
   bash ~/RetroPie/retropiemenu/Devils-Box.sh
   exit 1
