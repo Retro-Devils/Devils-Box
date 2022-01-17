a0f3a59cdfa98d8ee3558e8527cd64a60c5bee72
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-17 11:00:25 -0500

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -575,6 +575,7 @@ function audio() {
     case "$choice" in
     1) tampo ;;
     2) es-themes ;;
+    3) no-audio
     *) break ;;
     esac
   done
@@ -585,6 +586,27 @@ curl -sSL https://git.io/JDfjg | bash
 function es-themes () {
 sudo /home/pi/RetroPie-Setup/retropie_packages.sh retropiemenu launch /home/pi/RetroPie/retropiemenu/esthemes.rp
 }
+function no-audio () {
+sudo grep hdmi_force_edid_audio /boot/config.txt > /dev/null 2>&1if 
+[ $? -eq 0 ] ; then
+echo "The Audio changes have already been made. If you are still not getting audio then there must be another issue."
+sleep 4
+echo "For now I will reverse the changes since they did not help you."
+sleep 5
+sudo perl -p -i -e 's/hdmi_force_edid_audio=1/#dtoverlay=lirc-rpi/g' /boot/config.txt
+sudo perl -p -i -e 's/hdmi_force_hotplug=1/#hdmi_force_hotplug=1/g' /boot/config.txt
+sudo perl -p -i -e 's/hdmi_drive=2/#hdmi_drive=2/g' /boot/config.txt
+elseecho "I have scanned the config file and see that the audio fix is NOT in place."
+sleep 4
+echo "If this fix does not work please run the script again to roll the changes back."
+sleep 10
+echo "I will make the necessary config changes and reboot your Pi"
+sleep 5
+sudo perl -p -i -e 's/#dtoverlay=lirc-rpi/hdmi_force_edid_audio=1/g' /boot/config.txt
+sudo perl -p -i -e 's/#hdmi_force_hotplug=1/hdmi_force_hotplug=1/g' /boot/config.txt
+sudo perl -p -i -e 's/#hdmi_drive=2/hdmi_drive=2/g' /boot/config.txt
+sudo reboot
+fi
 ###-----------------------------------###
 ###  EMU TOOLS MENU FUNCTIONS   ###
 ###-----------------------------------###
