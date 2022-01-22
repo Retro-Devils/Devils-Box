dde32542b611eec0174dc839a40f9837355292f0
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-01-21 19:42:05 -0600

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -247,63 +247,65 @@ else
       3 "Amstrad CPC                      614MB     3264 GAMES" \
       4 "Arcade                           8.4GB     2361 GAMES" \
       5 "Arcadia                          201KB       47 GAMES" \
-      6 "Atari800                         770MB      650 GAMES" \
-      7 "Atari2600                        2.6MB      615 GAMES" \
-      8 "Atari5200                         70MB      080 GAMES" \
-      9 "Atari7800                          2MB       54 GAMES" \
-      10 "AtariLynx                         10MB       77 GAMES" \
-      11 "Atomiswave                       2.5GB       24 GAMES" \
-      12 "Commadore64                      9.5MB      144 GAMES" \
-      13 "Colecovision                     2.7MB      146 GAMES" \
-      14 "Dragon32                         897KB       51 GAMES" \
-      15 "Dreamcast                       12.7GB       19 GAMES" \
-      16 "Electron                         2.5MB      152 GAMES" \
-      17 "Famicon                           19MB      169 GAMES" \
-      18 "Famicom Disk                       2MB       43 GAMES" \
-      19 "Game and Watch                    48MB       53 GAMES" \
-      20 "Gameboy                           42MB      565 GAMES" \
-      21 "Gameboy Advance                  3.4GB     1006 GAMES" \
-      22 "Gameboy Color                    232MB      538 GAMES" \
-      23 "Gamegear                          42MB      249 GAMES" \
-      24 "Intellivision                      1MB       62 GAMES" \
-      25 "****Lightgun****                  ??GB       ?? GAMES" \
-      26 "MarkIII                            6MB       58 GAMES" \
-      27 "Mastersystem                      35MB      280 GAMES" \
-      28 "Genesis/Megadrive                409MB      561 GAMES" \
-      29 "Genesis/Megadrive-Japan          149MB      278 GAMES" \
-      30 "MSX                               30MB      708 GAMES" \
-      31 "MSX 2                           6.24MB       83 GAMES" \
-      32 "****Mugen****                    9.2GB      009 GAMES" \
-      33 "Nintendo 64                      5.0GB      338 GAMES" \
-      34 "Naomi                            1.5GB       15 GAMES" \
-      35 "Nintendo DS                        4GB      171 GAMES" \
-      36 "NeoGeo                           2.3GB      142 GAMES" \
-      37 "Neo Geo Pocket Color              21MB       40 GAMES" \
-      38 "Nintendo Entertainment System    100MB      869 GAMES" \
-      39 "Openbor                         1.84GB       37 GAMES" \
-      40 "Oric                             5.4MB      136 GAMES" \
-      41 "Pokemini                         5.4MB       44 GAMES" \
-      42 "PlayStation 1                      3GB       29 GAMES" \
-      43 "Sega Saturn                      108GB      303 GAMES" \
-      44 "Sega Staurn Japan                3.9GB       18 GAMES" \
-      45 "ScummVM                          2.5GB       21 GAMES" \
-      46 "Sega32x                           63MB       37 GAMES" \
-      47 "SegaCD                            11GB       52 GAMES" \
-      48 "Super Famicon                    475MB      487 GAMES" \
-      49 "SG-1000                            1MB       68 GAMES" \
-      50 "SNES                             508MB      603 GAMES" \
-      51 "SuperGraffiix                    2.4MB        5 GAMES" \
-      52 "Turbo Graffix 16                  20MB       94 GAMES" \
-      53 "Vectrex                          201KB       20 GAMES" \
-      54 "VideoPAC                         430KB       99 GAMES" \
-      55 "Virtual Boy                        8MB       24 GAMES" \
-      56 "Dreamcast VMU                      3MB      115 GAMES" \
-      57 "Wine                          SUB-MENU        9 GAMES" \
-      58 "Wonderswan Color                 116MB       84 GAMES" \
-      59 "Sharp X1                         7.6MB       69 GAMES" \
-      60 "Sharp X68000                     504MB      418 GAMES" \
-      61 "ZMachine                           4MB       30 GAMES" \
-      62 "ZXSpectrum                        38MB     1111 GAMES" \
+      6 "Astrocade                        174KB       48 GAMES" \
+      7 "Atari800                        5.21MB      100 GAMES" \
+      8 "Atari2600                        2.6MB      615 GAMES" \
+      9 "Atari5200                        928KB       81 GAMES" \
+      10 "Atari7800                          2MB       54 GAMES" \
+      11 "AtariLynx                         10MB       77 GAMES" \
+      12 "AtariST                           66MB      100 GAMES" \
+      13 "Atomiswave                       2.5GB       24 GAMES" \
+      14 "Commadore64                      9.5MB      144 GAMES" \
+      15 "Colecovision                     2.7MB      146 GAMES" \
+      16 "Dragon32                         897KB       51 GAMES" \
+      17 "Dreamcast                       12.7GB       19 GAMES" \
+      18 "Electron                         2.5MB      152 GAMES" \
+      19 "Famicon                           19MB      169 GAMES" \
+      20 "Famicom Disk                       2MB       43 GAMES" \
+      21 "Game and Watch                    48MB       53 GAMES" \
+      22 "Gameboy                           42MB      565 GAMES" \
+      23 "Gameboy Advance                  3.4GB     1006 GAMES" \
+      24 "Gameboy Color                    232MB      538 GAMES" \
+      25 "Gamegear                          42MB      249 GAMES" \
+      26 "Intellivision                      1MB       62 GAMES" \
+      27 "****Lightgun****                  ??GB       ?? GAMES" \
+      28 "MarkIII                            6MB       58 GAMES" \
+      29 "Mastersystem                      35MB      280 GAMES" \
+      30 "Genesis/Megadrive                409MB      561 GAMES" \
+      31 "Genesis/Megadrive-Japan          149MB      278 GAMES" \
+      32 "MSX                               30MB      708 GAMES" \
+      33 "MSX 2                           6.24MB       83 GAMES" \
+      34 "****Mugen****                    9.2GB      009 GAMES" \
+      35 "Nintendo 64                      5.0GB      338 GAMES" \
+      36 "Naomi                            1.5GB       15 GAMES" \
+      37 "Nintendo DS                        4GB      171 GAMES" \
+      38 "NeoGeo                           2.3GB      142 GAMES" \
+      39 "Neo Geo Pocket Color              21MB       40 GAMES" \
+      40 "Nintendo Entertainment System    100MB      869 GAMES" \
+      41 "Openbor                         1.84GB       37 GAMES" \
+      42 "Oric                             5.4MB      136 GAMES" \
+      43 "Pokemini                         5.4MB       44 GAMES" \
+      44 "PlayStation 1                      3GB       29 GAMES" \
+      45 "Sega Saturn                      108GB      303 GAMES" \
+      46 "Sega Staurn Japan                3.9GB       18 GAMES" \
+      47 "ScummVM                          2.5GB       21 GAMES" \
+      48 "Sega32x                           63MB       37 GAMES" \
+      49 "SegaCD                            11GB       52 GAMES" \
+      50 "Super Famicon                    475MB      487 GAMES" \
+      51 "SG-1000                            1MB       68 GAMES" \
+      52 "SNES                             508MB      603 GAMES" \
+      53 "SuperGraffiix                    2.4MB        5 GAMES" \
+      54 "Turbo Graffix 16                  20MB       94 GAMES" \
+      55 "Vectrex                          201KB       20 GAMES" \
+      56 "VideoPAC                         430KB       99 GAMES" \
+      57 "Virtual Boy                        8MB       24 GAMES" \
+      58 "Dreamcast VMU                      3MB      115 GAMES" \
+      59 "Wine                          SUB-MENU        9 GAMES" \
+      60 "Wonderswan Color                 116MB       84 GAMES" \
+      61 "Sharp X1                         7.6MB       69 GAMES" \
+      62 "Sharp X68000                     504MB      418 GAMES" \
+      63 "ZMachine                           4MB       30 GAMES" \
+      64 "ZXSpectrum                        38MB     1111 GAMES" \
       2>&1 >/dev/tty)
 
     case "$choice" in
@@ -312,11 +314,13 @@ else
     3) download-packs "amstradcpc" ;;
     4) download-packs "arcadia" ;;
     5) download-packs "arcade" ;;
-    6) download-packs-alt "atari800" ;;
-    7) download-packs "atari2600" ;;
-    8) download-packs-alt "atari5200" ;;
-    9) download-packs "atari7800" ;;
-    10) download-packs "atarilynx" ;;
+    6) download-packs-alt "arstrocade" ;;
+    7) download-packs-alt "atari800" ;;
+    8) download-packs "atari2600" ;;
+    9) download-packs-alt "atari5200" ;;
+    10) download-packs "atari7800" ;;
+    11) download-packs "atarilynx" ;;
+    12) download-packs-alt "atarist" ;;
     11) download-packs "atomiswave" ;;
     12) download-packs "c64" ;;
     13) download-packs "coleco" ;;
