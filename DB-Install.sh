d4926c20b6a1bd5618366442bf61de0e7977a092
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-28 00:04:51 -0600

    Update DB-Install.sh

--- DB-Install.sh
+++ DB-Install.sh
@@ -8,7 +8,7 @@ function main_menu() {
     local choice
 
     while true; do
-        choice=$(dialog --backtitle "$BACKTITLE" --title " DEVILS BOX 2.5 " \
+        choice=$(dialog --backtitle "$BACKTITLE" --title " DEVILS BOX 3.0 " \
             --ok-label Select --cancel-label Exit-Installer \
             --menu "
 DEVILS BOX RETROPIE INSTALLER" 25 40 40 \
