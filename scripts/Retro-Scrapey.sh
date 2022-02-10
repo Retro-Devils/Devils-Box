ab785b4b5d66cd3bfeda15aa3ea222db09d5cec7
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-09 22:59:43 -0600

    Update Retro-Scrapey.sh

--- scripts/Retro-Scrapey.sh
+++ scripts/Retro-Scrapey.sh
@@ -6,11 +6,9 @@ else
 fi
 function main_menu() {
 dialog  --sleep 1 --title "Retro Scrapey V:1.00" --msgbox "
-
 Retro Scrapey Info
 *So you can use your imported scrapes & scrape online easy. 
 *Made By The Retro Devils
-
 Featuring The Following: 
 --Scrapes to the following locations:
        BOXART   .../RetroPie/rom/system-name/boxart
@@ -36,6 +34,7 @@ local choice
       2 "System Select For Info Only " \
       3 "Scrape All Systems All Media Now" \
       4 "Scrape All Systems Info ONly Now" \
+      5 "Update Retro Scrapey" \
       2>&1 >/dev/tty)
 
     case "$choice" in
@@ -43,6 +42,7 @@ local choice
     2) scrape-info  ;;
     3) all-media    ;;
     4) all-info     ;;
+    5) up-scrapey   ;;
     *) break ;;
     esac
   done
@@ -54,95 +54,97 @@ local choice
     choice=$(dialog --backtitle "$BACKTITLE" --title "Retro Scrapey" \
      --ok-button Scrape --cancel-button Back \
      --menu "SELECT SYSTEM TO SCRAPE" 30 50 50 \
-      1 "AmstradCPC" \
-      2 "Arcade" \
-      3 "Atari800" \
-      4 "Atari2600" \
-      5 "Atari5200" \
-      6 "Atari7800" \
-      7 "Atari Lynx" \
-      8 "AtomisWave" \
-      9 "Daphne" \
-      10 "Dreamcast" \
-      11 "Final Burn Alpha" \
-      12 "Family Disk System" \
-      13 "GameGear" \
-      14 "Gameboy" \
-      15 "Gameboy Advance" \
-      16 "Gameboy Advance Hacks" \
-      17 "GenesisHacks" \
-      18 "GameGear Hacks" \
-      19 "MAME " \
-      20 "MAME Advance" \
-      21 "Mastersytem" \
-      22 "MegaDrive" \
-      23 "Model 3" \
-      24 "Mugens " \
-      25 "Nintendo 64 " \
-      26 "Naomi" \
-      27 "NintendoDS " \
-      28 "NeoGEo" \
-      29 "NES" \
-      30 "NES Hacks" \
-      31 "NeoGeo Pocket" \
-      32 "PC Engine" \
-      33 "Ports" \
-      34 "PSP" \
-      35 "Playstation" \
-      36 "Sega Saturn" \
-      37 "Sega32X" \
-      38 "SegaCD " \
-      39 "SNES" \
-      40 "Vectrex" \
-      41 "VirtualBoy" \
-      42 "Wine" \
-      43 "Zxspectrum" \
+      1 "Amiga" \
+      2 "AmstradCPC" \
+      3 "Arcade" \
+      4 "Atari800" \
+      5 "Atari2600" \
+      6 "Atari5200" \
+      7 "Atari7800" \
+      8 "Atari Lynx" \
+      9 "AtomisWave" \
+      10 "Daphne" \
+      11 "Dreamcast" \
+      12 "Final Burn Alpha" \
+      13 "Family Disk System" \
+      14 "GameGear" \
+      15 "Gameboy" \
+      16 "Gameboy Advance" \
+      17 "Gameboy Advance Hacks" \
+      18 "GenesisHacks" \
+      19 "GameGear Hacks" \
+      20 "MAME " \
+      21 "MAME Advance" \
+      22 "Mastersytem" \
+      23 "MegaDrive" \
+      24 "Model 3" \
+      25 "Mugens " \
+      26 "Nintendo 64 " \
+      27 "Naomi" \
+      28 "NintendoDS " \
+      29 "NeoGEo" \
+      30 "NES" \
+      31 "NES Hacks" \
+      32 "NeoGeo Pocket" \
+      33 "PC Engine" \
+      34 "Ports" \
+      35 "PSP" \
+      36 "Playstation" \
+      37 "Sega Saturn" \
+      38 "Sega32X" \
+      39 "SegaCD " \
+      40 "SNES" \
+      41 "Vectrex" \
+      42 "VirtualBoy" \
+      43 "Wine" \
+      44 "Zxspectrum" \
       2>&1 >/dev/tty)
 
     case "$choice" in
-      1) media "amstradcpc" ;;
-      2) media "arcade" ;;
-      3) media "atari800" ;;
-      4) media "atari2600" ;;
-      5) media "atari5200" ;;
-      6) media "atari7800" ;;
-      7) media "atarilynx" ;;
-      8) media "atomiswave" ;;
-      9) media "daphne" ;;
-      10) media "dreamcast" ;;
-      11) media "fba" ;;
-      12) media "fds" ;;
-      13) media "gamegear" ;;
-      14) media "gb" ;;
-      15) media "gba" ;;
-      16) media "gbah" ;;
-      17) media "genh" ;;
-      18) media "ggh" ;;
-      19) media "mame" ;;
-      20) media "mame-advmame" ;;
-      21) media "mastersytem" ;;
-      22) media "megaDrive" ;;
-      23) media "model3" ;;
-      24) media "mugens" ;;
-      25) media "n64" ;;
-      26) media "naomi" ;;
-      27) media "nds" ;;
-      28) media "neogeo" ;;
-      29) media "nes" ;;
-      30) media "nesh" ;;
-      31) media "ngp" ;;
-      32) media "pce" ;;
-      33) media "ports" ;;
-      34) media "psp" ;;
-      35) media "psx" ;;
-      36) media "saturn" ;;
-      37) media "sega32x" ;;
-      38) media "segacd" ;;
-      39) media "snes" ;;
-      40) media "vectrex" ;;
-      41) media "virtualBoy" ;;
-      42) media "wine" ;;
-      43) media "zxspectrum" ;;
+      1) media "amiga" ;;
+      2) media "amstradcpc" ;;
+      3) media "arcade" ;;
+      4) media "atari800" ;;
+      5) media "atari2600" ;;
+      6) media "atari5200" ;;
+      7) media "atari7800" ;;
+      8) media "atarilynx" ;;
+      9) media "atomiswave" ;;
+      10) media "daphne" ;;
+      11) media "dreamcast" ;;
+      12) media "fba" ;;
+      13) media "fds" ;;
+      14) media "gamegear" ;;
+      15) media "gb" ;;
+      16) media "gba" ;;
+      17) media "gbah" ;;
+      18) media "genh" ;;
+      19) media "ggh" ;;
+      20) media "mame" ;;
+      21) media "mame-advmame" ;;
+      22) media "mastersytem" ;;
+      23) media "megaDrive" ;;
+      24) media "model3" ;;
+      25) media "mugens" ;;
+      26) media "n64" ;;
+      27) media "naomi" ;;
+      28) media "nds" ;;
+      29) media "neogeo" ;;
+      30) media "nes" ;;
+      31) media "nesh" ;;
+      32) media "ngp" ;;
+      33) media "pce" ;;
+      34) media "ports" ;;
+      35) media "psp" ;;
+      36) media "psx" ;;
+      37) media "saturn" ;;
+      38) media "sega32x" ;;
+      39) media "segacd" ;;
+      40) media "snes" ;;
+      41) media "vectrex" ;;
+      42) media "virtualBoy" ;;
+      43) media "wine" ;;
+      44) media "zxspectrum" ;;
     *) break ;;
     esac
   done
@@ -154,95 +156,97 @@ local choice
     choice=$(dialog --backtitle "$BACKTITLE" --title "Retro Scrapey" \
      --ok-button Scrape --cancel-button Back \
      --menu "SELECT SYSTEM TO SCRAPE" 30 50 50 \
-      1 "AmstradCPC" \
-      2 "Arcade" \
-      3 "Atari800" \
-      4 "Atari2600" \
-      5 "Atari5200" \
-      6 "Atari7800" \
-      7 "Atari Lynx" \
-      8 "AtomisWave" \
-      9 "Daphne" \
-      10 "Dreamcast" \
-      11 "Final Burn Alpha" \
-      12 "Family Disk System" \
-      13 "GameGear" \
-      14 "Gameboy" \
-      15 "Gameboy Advance" \
-      16 "Gameboy Advance Hacks" \
-      17 "GenesisHacks" \
-      18 "GameGear Hacks" \
-      19 "MAME " \
-      20 "MAME Advance" \
-      21 "Mastersytem" \
-      22 "MegaDrive" \
-      23 "Model 3" \
-      24 "Mugens " \
-      25 "Nintendo 64 " \
-      26 "Naomi" \
-      27 "NintendoDS " \
-      28 "NeoGEo" \
-      29 "NES" \
-      30 "NES Hacks" \
-      31 "NeoGeo Pocket" \
-      32 "PC Engine" \
-      33 "Ports" \
-      34 "PSP" \
-      35 "Playstation" \
-      36 "Sega Saturn" \
-      37 "Sega32X" \
-      38 "SegaCD " \
-      39 "SNES" \
-      40 "Vectrex" \
-      41 "VirtualBoy" \
-      42 "Wine" \
-      43 "Zxspectrum" \
+      1 "Amiga" \
+      2 "AmstradCPC" \
+      3 "Arcade" \
+      4 "Atari800" \
+      5 "Atari2600" \
+      6 "Atari5200" \
+      7 "Atari7800" \
+      8 "Atari Lynx" \
+      9 "AtomisWave" \
+      10 "Daphne" \
+      11 "Dreamcast" \
+      12 "Final Burn Alpha" \
+      13 "Family Disk System" \
+      14 "GameGear" \
+      15 "Gameboy" \
+      16 "Gameboy Advance" \
+      17 "Gameboy Advance Hacks" \
+      18 "GenesisHacks" \
+      19 "GameGear Hacks" \
+      20 "MAME " \
+      21 "MAME Advance" \
+      22 "Mastersytem" \
+      23 "MegaDrive" \
+      24 "Model 3" \
+      25 "Mugens " \
+      26 "Nintendo 64 " \
+      27 "Naomi" \
+      28 "NintendoDS " \
+      29 "NeoGEo" \
+      30 "NES" \
+      31 "NES Hacks" \
+      32 "NeoGeo Pocket" \
+      33 "PC Engine" \
+      34 "Ports" \
+      35 "PSP" \
+      36 "Playstation" \
+      37 "Sega Saturn" \
+      38 "Sega32X" \
+      39 "SegaCD " \
+      40 "SNES" \
+      41 "Vectrex" \
+      42 "VirtualBoy" \
+      43 "Wine" \
+      44 "Zxspectrum" \
       2>&1 >/dev/tty)
 
     case "$choice" in
-      1) info "amstradcpc" ;; 
-      2) info "arcade" ;;
-      3) info "atari800" ;;
-      4) info "atari2600" ;;
-      5) info "atari5200" ;;
-      6) info "atari7800" ;;
-      7) info "atarilynx" ;;
-      8) info "atomiswave" ;;
-      9) info "daphne" ;;
-      10) info "dreamcast" ;;
-      11) info "fba" ;;
-      12) info "fds" ;;
-      13) info "gamegear" ;;
-      14) info "gb" ;;
-      15) info "gba" ;;
-      16) info "gbah" ;;
-      17) info "genh" ;;
-      18) info "ggh" ;;
-      19) info "mame" ;;
-      20) info "mame-advmame" ;;
-      21) info "mastersytem" ;;
-      22) info "megadrive" ;;
-      23) info "model3" ;;
-      24) info "mugens" ;;
-      25) info "n64" ;;
-      26) info "naomi" ;;
-      27) info "nds" ;;
-      28) info "neogeo" ;;
-      29) info "nes" ;;
-      30) info "nesh" ;;
-      31) info "ngp" ;;
-      32) info "pce" ;;
-      33) info "ports" ;;
-      34) info "psp" ;;
-      35) info "psx" ;;
-      36) info "saturn" ;;
-      37) info "sega32x" ;;
-      38) info "segacd " ;;
-      39) info "snes" ;;
-      40) info "vectrex" ;;
-      41) info "virtualBoy" ;;
-      42) info "wine" ;;
-      43) info "zxspectrum" ;;
+      1) info "amiga" ;;
+      2) info "amstradcpc" ;; 
+      3) info "arcade" ;;
+      4) info "atari800" ;;
+      5) info "atari2600" ;;
+      6) info "atari5200" ;;
+      7) info "atari7800" ;;
+      8) info "atarilynx" ;;
+      9) info "atomiswave" ;;
+      10) info "daphne" ;;
+      11) info "dreamcast" ;;
+      12) info "fba" ;;
+      13) info "fds" ;;
+      14) info "gamegear" ;;
+      15) info "gb" ;;
+      16) info "gba" ;;
+      17) info "gbah" ;;
+      18) info "genh" ;;
+      19) info "ggh" ;;
+      20) info "mame" ;;
+      21) info "mame-advmame" ;;
+      22) info "mastersytem" ;;
+      23) info "megadrive" ;;
+      24) info "model3" ;;
+      25) info "mugens" ;;
+      26) info "n64" ;;
+      27) info "naomi" ;;
+      28) info "nds" ;;
+      29) info "neogeo" ;;
+      30) info "nes" ;;
+      31) info "nesh" ;;
+      32) info "ngp" ;;
+      33) info "pce" ;;
+      34) info "ports" ;;
+      35) info "psp" ;;
+      36) info "psx" ;;
+      37) info "saturn" ;;
+      38) info "sega32x" ;;
+      39) info "segacd " ;;
+      40) info "snes" ;;
+      41) info "vectrex" ;;
+      42) info "virtualBoy" ;;
+      43) info "wine" ;;
+      44) info "zxspectrum" ;;
     *) break ;;
     esac
   done
@@ -268,6 +272,15 @@ function all-info() {
 clear
 cd "$HOME"/RetroPie/roms/
 /opt/retropie/supplementary/scraper/scraper -img_format=png -image_dir="./boxart" -image_path="./boxart" -download_images=false -image_suffix="" -marquee_dir="./wheel" -marquee_path="./wheel" -download_marquees=false -marquee_suffix="" -video_dir="./snap" -video_path="./snap" -download_videos=false -video_suffix="" -refresh -console_src gdb,ss,ovgdb
+}
+function up-scrapey() {
+echo "---UPDATING RETRO SCRAPEY NOW---"
+sleep 5
+sudo rm -r "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+sudo rm -r /usr/local/bin/scrapey
+wget https://raw.githubusercontent.com/Retro-Devils/Retro-Scrapey/main/Retro-Scrapey.sh -P "$HOME"/RetroPie/retropiemenu/
+chmod 755 "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh
+sudo cp "$HOME"/RetroPie/retropiemenu/Retro-Scrapey.sh -f /usr/local/bin/scrapey
 }
 
 main_menu
