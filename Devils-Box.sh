178bce4b55458340558984b4857f135b5cd699e5
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-17 22:20:10 -0600

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -55,7 +55,7 @@ function main_menu() {
   fi
     choice=(dialog --backtitle "$BACKTITLE" --title "MAIN MENU " \
       --ok-label Select --cancel-label Exit-Devils-Box \
-      --menu "DEVILS BOX V3.05--------------UPDATED 2/16/22" 20 50 30 )
+      --menu "DEVILS BOX V3.05--------------UPDATED 2/17/22" 20 50 30 )
     if [ "$DB_STATUS" == 1 ]; then
       options=( \
       - "<---->Downloaders<---->"
