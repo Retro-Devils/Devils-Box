49243e5888c9e523a73865ba265280e8878aa785
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-01 19:00:36 -0600

    Add files via upload

---
+++ scripts/MSU1 Setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+dialog  --sleep 1 --title "SNES MSU1 SETUP" --msgbox " 
+<---------------WHATS THIS DO?-------------------->
+- COPIES YOUR SNES CONFIGS TO NEW SNESMSU1 FOLDER
+- MAKES SURE YOU HAVE CORRECT EMU INSTALLED 
+- DOWNLOADS A FEW NEEDED FILES FOR MSU1" 0 0
+clear
+echo "<----Checking For Emu Now---->"
+sleep 2
+if [ ! -d /opt/retropie/libretrocores/lr-snes9x ]; then
+dialog  --sleep 1 --title "LR-SNES9X NOT INSTALLED !! " --msgbox " 
+Taking you to Retropie Setup
+GO TO-- Manage Main Pacakges/lr-snes9x
+Presss A To Install" 0 0
+sudo "$HOME"/RetroPie-Setup/retropie_setup.sh;
+else
+echo "<----Snes9X Installed Begining Setup Now---->"
+sleep 2
+mkdir "$HOME"/RetroPie/roms/snesmsu1
+sudo cp -r /opt/retropie/configs/snes -d /opt/retropie/configs/snesmsu1
+wget 
+dialog  --sleep 1 --title "MSU1 SETUP COMPLETE" --msgbox " 
+<-------------ATTENTION------------------ATTENTION ----------------ATTENTION-------------ATTENTION-------------->
+For this to work you need to edit es system comfig
+Located at /home/pi/.emulationstation/es_system.cfg
+Please add the following
+  <system>
+    <name>snesmsu1</name>
+    <fullname>Super Nintendo MSU1</fullname>
+    <path>/home/pi/RetroPie/roms/snesmsu1</path>
+    <extension>.7z .bin .bs .smc .sfc .fig .swc .mgd .zip .7Z .BIN .BS .SMC .SFC .FIG .SWC .MGD .ZIP</extension>
+    <command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ snesmsu1 %ROM%</command>
+    <platform>snes</platform>
+    <theme>snesmsu1</theme>
+  </system>" 0 0
+fi
+
