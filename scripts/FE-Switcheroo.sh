04cf12f8825ea07effb29365a1f68f3d364ac929
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-03 21:38:05 -0600

    Update FE-Switcheroo.sh

--- scripts/FE-Switcheroo.sh
+++ scripts/FE-Switcheroo.sh
@@ -323,40 +323,23 @@ read -n 1 -s -r -p "Above Is Converting Results-----Press any key to Continue"
 }
 
 function update-switcheroo() {
-if [ -f "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh ]; then
+clear
 echo "---UPDATING FE SWITCHEROO NOW---"
 sleep 5
 sudo rm -r "$HOME"/RetroPie/retropiemenu/icons/FE-Switcheroo.png
 sudo rm -R "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
 sudo rm -R /usr/local/bin/switcheroo
 sudo rm -r "$HOME"/.attract/tools/FE-Switcheroo.sh
-wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
-wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
+sudo rm -r "$HOME"/Devils-Box/scripts/FE-Switcheroo.sh
+wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/Devils-Box/scripts/
 sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
-chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+cp "$HOME"/Devils-Box/scripts/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
 chmod 755 "$HOME"/.attract/tools/FE-Switcheroo.sh
 sudo chmod 755 /usr/local/bin/switcheroo
-cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
-echo "UPDATE COMPLETE LAUNCHING SWITCHEROO"
-"$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+echo "UPDATE COMPLETE"
 sleep 2 
-else
-echo "---INSTALLING FE SWITCHEROO NOW---"
-sleep 5
-wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
-sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
-wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
-chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
-sudo chmod 755 /usr/local/bin/switcheroo
-mkdir "$HOME"/.attract/tools/
-cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
-cp "$HOME"/RetroPie/retropiemenu/Devils-Box.sh -P "$HOME"/.attract/tools/
-chmod 755 "$HOME"/.attract/tools/
-echo "INSTALL COMPLETE LAUNCHING SWITCHEROO"
-sleep 2
-"$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
-fi
 }
+
 fe_menu
   fi
 fi
