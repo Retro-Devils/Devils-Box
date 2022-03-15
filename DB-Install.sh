638bc81baa69a8e32053a9084b117edc2f89f65c
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-14 19:02:28 -0500

    Update DB-Install.sh

--- DB-Install.sh
+++ DB-Install.sh
@@ -37,6 +37,8 @@ cp $HOME/Devils-Box/Devils-Box.sh -f $HOME/RetroPie/retropiemenu/
 cp $HOME/Devils-Box/files/images/Devils-Box.png -f $HOME/RetroPie/retropiemenu/icons/
 sudo cp $HOME/Devils-Box/files/box -f /usr/local/bin/
 sudo cp $HOME/Devils-Box/Devils-Box.sh -f /usr/local/bin/Devils-Box
+wget https://github.com/Retro-Devils/Devils-Pi/blob/main/things -P /usr/local/bin/confirm
+sudo chmod 755 /usr/local/bin/confirm
 sudo chmod 755 /usr/local/bin/box
 chmod 755 $HOME/RetroPie/retropiemenu/Devils-Box.sh
 sudo chmod 755 /usr/local/bin/Devils-Box
