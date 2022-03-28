4309bb6db5031b71256e539566495db54e4f4598
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-27 23:18:21 -0400

    Update FE-Switcheroo.sh

--- scripts/FE-Switcheroo.sh
+++ scripts/FE-Switcheroo.sh
@@ -16,7 +16,8 @@ function fe_menu() {
             4 "use PEGASUS FE as FRONTEND" \
 	    + "<--------->HELPERS<---------->" \
 	    5 "Attract Mode WORK IN PROGRESS" \
-	    6 "Pegasus FE Helper" \
+	    6 "Emulation Station Helper" \
+	    7 "Pegasus FE Helper" \
 	    + "<--------->UPDATE<----------->" \
 	    7 "Update FE Switcheroo" \
             2>&1 > /dev/tty)
@@ -28,8 +29,9 @@ function fe_menu() {
             3) ES_FE ;;
             4) PE_FE ;;
 	    5) attract_helper ;;
-   	    6) pegasus_helper ;;
-	    7) update-switcheroo ;;
+	    6) es_helper ;;
+   	    7) pegasus_helper ;;
+	    8) update-switcheroo ;;
             *)  break ;;
         esac
     done
@@ -169,6 +171,67 @@ elif grep -q 'startx \#auto' "$AUTOSTART"; then
 	fe="Desktop Mode"
 fi
 }
+
+function es_helper() {
+local choice
+
+  while true; do
+    choice=$(dialog --backtitle "$BACKTITLE" --title "ES HELPER " \
+     --ok-button Select --cancel-button Exit \
+     --menu "Version 0.9" 15 30 30 \
+      1 "Devils Themes" \
+      2 "ES Theme Menu" \
+      3 "Hurstys Themes" \
+       2>&1 >/dev/tty)
+    
+    case "$choice" in 
+    1) devils-themes;;
+    2) es-themes  ;;
+    3) hursty-themes ;;
+    *) break ;;
+    esac
+  done
+}
+
+function es-themes () {
+sudo /home/pi/RetroPie-Setup/retropie_packages.sh retropiemenu launch /home/pi/RetroPie/retropiemenu/esthemes.rp
+}
+function hursty-themes () {
+wget https://raw.githubusercontent.com/RetroHursty69/HurstyThemes/master/install.sh
+chmod +x "install.sh"
+./install.sh
+}
+
+function devil-themes() {
+local choice 
+
+  while true; do
+    choice=$(dialog --backtitle "$BACKTITLE" --title " DEVILS THEMES MENU " \ 
+    --ok-label Select --cancel-label Main-Menu \ 
+    --menu "SELECT THEME AND PRESS A " 20 50 30 \ 
+     1 "Devil Chromey " \ 
+     2 "Neon I.B.A.D " \ 
+     2>&1 >/dev/tty) 
+     
+   case "$choice" in 
+   1) devil-chromey ;; 
+   2) neon-ibad ;; 
+   *) break ;; 
+   esac 
+ done
+ }
+ 
+ function devil-chromey() {
+ wget https://archive.org/download/devils-themes/devil-chromey.zip -P ${HOME}/
+ unzip -o "$HOME"/devil-chromey.zip -d /home/pi/.emulationstation/themes/
+ sudo rm -R "$HOME"/devil-chromey.zip
+ } 
+ 
+ function neon-ibad() {
+ wget https://archive.org/download/devils-themes/NEON-IBAD.zip -P ${HOME}/
+ unzip -o "$HOME"/NEON-IBAD.zip -d /home/pi/.emulationstation/themes/
+ sudo rm -R "$HOME"/NEON-IBAD.zip}
+
 
 function pegasus_helper() {
 local choice
