2b0e579a7d852902304e830f54c6e9b8e336759e
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-27 23:28:56 -0400

    Update BIOS.sh

--- scripts/BIOS.sh
+++ scripts/BIOS.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export NCURSES_NO_UTF8_ACS=1
 
-HOST3="https://archive.org/download/devils-bios"
+HOSTB="https://archive.org/download/devils-bios"
 
 clear
-wget -m -r -np -nH -nd -R "index.html" "${HOST3}"/BIOS/ -P "$HOME"/RetroPie/BIOS/ -erobots=off
+wget -m -r -np -nH -nd -R "index.html" "${HOSTB}"/BIOS/ -P "$HOME"/RetroPie/BIOS/ -erobots=off
 rm -f "$HOME"/RetroPie/BIOS/index.html.tmp
 
