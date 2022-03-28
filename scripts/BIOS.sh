e9afdbc296bfb196bf32c86e2839c31f2ed40283
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-27 23:24:18 -0400

    Create BIOS.sh

---
+++ scripts/BIOS.sh
@@ -0,0 +1,7 @@
+
+HOST3="https://archive.org/download/devils-bios"
+
+clear
+wget -m -r -np -nH -nd -R "index.html" "${HOST3}"/BIOS/ -P "$HOME"/RetroPie/BIOS/ -erobots=off
+rm -f "$HOME"/RetroPie/BIOS/index.html.tmp
+
