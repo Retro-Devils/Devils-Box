c02d13d678944a34de10508c10c9f4acf49f50ad
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-04-04 16:05:27 -0500

    Update launch-videos.sh

--- scripts/launch-videos.sh
+++ scripts/launch-videos.sh
@@ -2,13 +2,13 @@
 echo "Getting Devils Launch Video & Setting Up"
 wget INSERT VIDEO START LINK -p home/pi/etc/emulationstation/videos/
 sleep 3
-sudo cat <<\EOF55 > "/home/pi/etc/emulationstation/runcommandonstart.sh"
+sudo cat <<\EOF55 > "/home/pi/etc/emulationstation/runcommand-onstart.sh"
 omxplayer home/pi/etc/emulationstation/videos/start.mp4
 EOF55
 
 echo "Getting Devils Exit Video & Setting Up"
 wget INSERT VIDEO END LINK -p home/pi/etc/emulationstation/videos/
 sleep 3
-sudo cat <<\EOF45 > "/home/pi/etc/emulationstation/runcommandonend.sh"
+sudo cat <<\EOF45 > "/home/pi/etc/emulationstation/runcommand-onend.sh"
 omxplayer home/pi/etc/emulationstation/videos/end.mp4
 EOF45
