5c7659bdf7758cfb8ab506ed0572f92654b0b08f
ALLRiPPED <37816162+ALLRiPPED@users.noreply.github.com>
2022-05-07 22:46:24 -0500

    Update Remove.sh

--- scripts/Remove.sh
+++ scripts/Remove.sh
@@ -6,7 +6,7 @@ local choice
 
   while true; do
   choice=$(dialog --backtitle "$BACKTITLE" --title " REMOVE PACKS MENU" \
-      --ok-label Remove --cancel-label Game Tools \
+      --ok-label Remove --cancel-label "Game Tools" \
       --menu "PRESS A/ENTER TO REMOVE PACK" 30 70 50 \
       1 "Remove Amiga" \
       2 "Remove AmigaCD32" \
