acee4f56de384582242308493e4c4a7ec4384f0c
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-09-06 14:26:55 -0700

    Update Packs.sh

--- scripts/Packs.sh
+++ scripts/Packs.sh
@@ -219,7 +219,6 @@ clear
 wget -m -r -np -nH -nd -R "index.html" "${HOST1}"/"${1}"/ -P "$HOME"/RetroPie/roms/"${1}" -erobots=off
 rm -f "$HOME"/RetroPie/roms/"${1}"/index.html.tmp
 fi
-fi
 }
 
 function download-packs-alt() {
