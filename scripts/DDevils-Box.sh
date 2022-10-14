13ceadc59d183c4558cc5d711181268dffb2a371
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 13:10:49 -0400

    Update DDevils-Box.sh

--- scripts/DDevils-Box.sh
+++ scripts/DDevils-Box.sh
@@ -1,10 +1,10 @@
 rpi=$(cat /proc/device-tree/model |awk '{print $3}')
 
-if [ "$rpi" = "Zero" ]; then bash /{$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
-elif [ "$rpi" = "1" ]; then bash /{$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
-elif [ "$rpi" = "2" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
-elif [ "$rpi" = "3" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
-elif [ "$rpi" = "4" ]; then bash /{$HOME}/devils-Box/scripts/PI-4-Devils-Box.sh
+if [ "$rpi" = "Zero" ]; then bash {$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
+elif [ "$rpi" = "1" ]; then bash {$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
+elif [ "$rpi" = "2" ]; then bash {$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
+elif [ "$rpi" = "3" ]; then bash {$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
+elif [ "$rpi" = "4" ]; then bash {$HOME}/devils-Box/scripts/PI-4-Devils-Box.sh
 else
-/{$HOME}/devils-Box/scripts/Multi-Devils-Box.sh
+{$HOME}/devils-Box/scripts/Multi-Devils-Box.sh
 fi
