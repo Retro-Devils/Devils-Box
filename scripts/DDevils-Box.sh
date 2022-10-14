9f2d1d13ea1ffa0afbf692624b4f2797c820cd1c
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 13:08:49 -0400

    Update DDevils-Box.sh

--- scripts/DDevils-Box.sh
+++ scripts/DDevils-Box.sh
@@ -5,4 +5,6 @@ elif [ "$rpi" = "1" ]; then bash /{$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
 elif [ "$rpi" = "2" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
 elif [ "$rpi" = "3" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
 elif [ "$rpi" = "4" ]; then bash /{$HOME}/devils-Box/scripts/PI-4-Devils-Box.sh
+else
+/{$HOME}/devils-Box/scripts/Multi-Devils-Box.sh
 fi
