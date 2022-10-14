88caec2fa3243b7e440219db11b605da32009c0e
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 15:58:54 -0600

    Update PI-3-Devils-Box.sh

--- scripts/PI-3/PI-3-Devils-Box.sh
+++ scripts/PI-3/PI-3-Devils-Box.sh
@@ -107,7 +107,7 @@ if [ $NETCHECK -eq 1 ]; then
 dialog  --sleep 1 --title "OFFLINE ERROR!!" --msgbox " 
 Offline ... Downloads not Availible Please Connect To Internet!" 0 0
   else
-bash "$HOME"/Devils-Box/scripts/Homebrews.sh
+bash "$HOME"/Devils-Box/scripts/PI-3/PI-3-Homebrews.sh
 fi
 }
 
