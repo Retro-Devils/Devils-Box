9ca1e4476784544414f47bb16801ce4da9bfd70a
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 16:05:59 -0600

    Update Multi-Devils-Box.sh

--- scripts/Multi-Devils-Box.sh
+++ scripts/Multi-Devils-Box.sh
@@ -37,7 +37,7 @@ function main_menu() {
       5 "Pick & Choose"
       6 "Translations Packs"
       - "<---Devils Box Tools---->"
-	  7 "Devils Box Tools" 
+      7 "Devils Box Tools" 
       8 "Devils Box Credits"
       9 "DB Games Server Check"
       - "<----------------------->"
@@ -107,7 +107,7 @@ if [ $NETCHECK -eq 1 ]; then
 dialog  --sleep 1 --title "OFFLINE ERROR!!" --msgbox " 
 Offline ... Downloads not Availible Please Connect To Internet!" 0 0
   else
-bash "$HOME"/Devils-Box/scripts/Homebrews.sh
+bash "$HOME"/Devils-Box/scripts/PI-4/PI-4-Homebrews.sh
 fi
 }
 
@@ -127,7 +127,7 @@ if [ $NETCHECK -eq 1 ]; then
 dialog  --sleep 1 --title "OFFLINE ERROR!!" --msgbox " 
 Offline ... Downloads not Availible Please Connect To Internet!" 0 0
   else
-bash "$HOME"/Devils-Box/scripts/Translations.sh
+bash "$HOME"/Devils-Box/scripts/PI-4-Translations.sh
 fi
 }
 
