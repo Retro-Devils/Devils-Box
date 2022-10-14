3d9da96acc6815671fced81411057284fbc38e5d
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 13:03:46 -0400

    Update and rename PI3-Devils-Box.sh to DDevils-Box.sh

---
+++ scripts/DDevils-Box.sh
@@ -0,0 +1,8 @@
+rpi=$(cat /proc/device-tree/model |awk '{print $3}')
+
+if [ "$rpi" = "Zero" ]; then bash /{$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
+elif [ "$rpi" = "1" ]; then bash /{$HOME}/devils-Box/scripts/PI-0-Devils-Box.sh
+elif [ "$rpi" = "2" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
+elif [ "$rpi" = "3" ]; then bash /{$HOME}/devils-Box/scripts/PI-3-Devils-Box.sh
+elif [ "$rpi" = "4" ]; then bash /{$HOME}/devils-Box/scripts/PI-4-Devils-Box.sh
+fi
