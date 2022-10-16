9f2ad69f3b9ff0fa5a54c46720809199174ae1dd
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-16 06:54:48 -0600

    Update Devils-Box.sh

--- Devils-Box.sh
+++ Devils-Box.sh
@@ -5,7 +5,7 @@ export NCURSES_NO_UTF8_ACS=1
 rpi=$(cat /proc/device-tree/model |awk '{print $3}')
 
 if [ "$rpi" = "Zero" ]; then bash $HOME/Devils-Box/scripts/PI-0/PI-0-Devils-Box.sh
-elif [ "$rpi" = "1" ]; then bash $HOME/Devils-Box/scripts/PI-0/PI-0-Devils-Box.sh
+elif [ "$rpi" = "Model B" ]; then bash $HOME/Devils-Box/scripts/PI-0/PI-0-Devils-Box.sh
 elif [ "$rpi" = "2" ]; then bash $HOME/Devils-Box/scripts/PI-3/PI3-Devils-Box.sh
 elif [ "$rpi" = "3" ]; then bash $HOME/Devils-Box/scripts/PI-3/PI-3-Devils-Box.sh
 elif [ "$rpi" = "4" ]; then bash $HOME/Devils-Box/scripts/PI-4/PI-4-Devils-Box.sh
