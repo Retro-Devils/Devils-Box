6133f65bddf5009addf2a9cd2f8097d53b2fffb0
ALLRiPPED <thepitster@gmail.com>
2022-12-18 11:35:39 -0600

    Update PI-3-PickChoose.sh

--- scripts/PI-3/PI-3-PickChoose.sh
+++ scripts/PI-3/PI-3-PickChoose.sh
@@ -7,7 +7,7 @@ DB_SETTINGS="$HOME/.devilsbox/db_settings.ini"
 HOST1="https://archive.org/download/the-devils-box-alt"
 HOST2="https://archive.org/download/the-devils-box_202112"
 HOST3="https://archive.org/download/devils-bios"
-HOST4="https://archive.org/download/devils-wine"
+HOST4="https://archive.org/download/devils-wine_20221218"
 HOST5="https://archive.org/download/devils-dos"
 #---------------NON-DEVILS-HOSTS-----------------------#
 HOST6="https://archive.org/download/PSP_US_Arquivista"
