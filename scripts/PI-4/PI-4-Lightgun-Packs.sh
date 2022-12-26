d0afbd2146f34d935f094ba5d95f562570f838f3
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-12-26 13:12:48 -0700

    Update PI-4-Lightgun-Packs.sh

--- scripts/PI-4/PI-4-Lightgun-Packs.sh
+++ scripts/PI-4/PI-4-Lightgun-Packs.sh
@@ -57,19 +57,15 @@ fi
 }
 
 function nes-pack() {
-if [ ! -d "$HOME/RetroPie/roms/nes/Gun-Games" ]; then sudo mkdir "$HOME"/RetroPie/roms/nes/Gun-Games/ ; fi
-wget -m -r -np -nH -nd -R "index.html" https://archive.org/download/RPI-Lightgun-Games/zips/NES-Gun-Games.zip -P "$HOME"/RetroPie/roms/nes/Gun-Games/ -erobots=off
-unzip "$HOME"/RetroPie/roms/nes/Gun-Games/NES-Gun-Games.zip
-sudo rm "$HOME"/RetroPie/roms/nes/Gun-Games/NES-Gun-Games.zip
-rm -f "$HOME"/RetroPie/roms/nes/Gun-Games/index.html.tmp
+wget -m -r -np -nH -nd -R "index.html" https://archive.org/download/RPI-Lightgun-Games/zips/NES-Gun-Games.zip -P "$HOME"/RetroPie/roms/nes/ -erobots=off
+unzip "$HOME"/RetroPie/roms/nes/NES-Gun-Games.zip
+sudo rm "$HOME"/RetroPie/roms/nes/NES-Gun-Games.zip
 }
 
 function snes-pack() {
-if [ ! -d "$HOME/RetroPie/roms/snes/Gun-Games" ]; then sudo mkdir "$HOME"/RetroPie/roms/nes/Gun-Games/ ; fi
-wget -m -r -np -nH -nd -R "index.html" https://archive.org/download/RPI-Lightgun-Games/zips/SNES-Gun-Games.zip -P "$HOME"/RetroPie/roms/snes/Gun-Games/ -erobots=off
-unzip "$HOME"/RetroPie/roms/snes/Gun-Games/SNES-Gun-Games.zip
-sudo rm "$HOME"/RetroPie/roms/snes/Gun-Games/SNES-Gun-Games.zip
-rm -f "$HOME"/RetroPie/roms/snes/Gun-Games/Gun-Games/index.html.tmp
+wget -m -r -np -nH -nd -R "index.html" https://archive.org/download/RPI-Lightgun-Games/zips/SNES-Gun-Games.zip -P "$HOME"/RetroPie/roms/snes/ -erobots=off
+unzip "$HOME"/RetroPie/roms/snes/SNES-Gun-Games.zip
+sudo rm "$HOME"/RetroPie/roms/snes/SNES-Gun-Games.zip
 }
 
 lightgun_menu
