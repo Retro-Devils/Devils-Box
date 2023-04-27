179119ecda4c62b71b4cb7edceba5c0432185abf
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2023-04-27 14:18:57 -0600

    Update Tester-Menu.sh

--- scripts/Tester-Menu.sh
+++ scripts/Tester-Menu.sh
@@ -9,24 +9,22 @@ local choice
      --ok-button Select --cancel-button Back \
      --menu " TEST SOMETHING " 30 70 50 \
       + "--FINISHED OR CLOSE TO IT--" \
-      1 "RPI Lightgun Tool" \
       + "--UNFINISHED/NOT WORKING--" \
-      2 "Collections Helper " \
-      3 "Icon Helper " \
-      4 "Pick & Choose Ports" \
+      1 "Collections Helper " \
+      2 "Icon Helper " \
+      3 "Pick & Choose Ports" \
       + "--------------- " \
-      5 "Update Testers Menu" \
+      4 "Update Testers Menu" \
       2>&1 >/dev/tty)
 
     case "$choice" in
     +) none ;;
-    1) rpi-lg-tool ;;
-    2) bash "$HOME"/Devils-Box/helpers/emulationstation/Collections.sh ;;
-    3) bash "$HOME"/Devils-Box/helpers/emulationstation/Icon-Helper.sh ;;
+    1) bash "$HOME"/Devils-Box/helpers/emulationstation/Collections.sh ;;
+    2) bash "$HOME"/Devils-Box/helpers/emulationstation/Icon-Helper.sh ;;
     +) none ;;
-    4) ports ;;
+    3) ports ;;
     +) no ;;
-    5) update-test ;;
+    4) update-test ;;
     *) break ;;
     esac
   done
